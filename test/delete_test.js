6f11de42c14297a918c1a6ab467fad234d492070
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2024-02-26 08:56:16 +0200

    add read and delete tests

---
+++ test/delete_test.js
@@ -0,0 +1,30 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Deleting a user", () => {
+  let joe;
+
+  beforeEach((done) => {
+    joe = new User({ name: "Joe" });
+
+    joe.save().then(() => done());
+  });
+
+  // it("modal instance remove", (done) => {
+  //   joe.remove({}).then(() => { //! remove is not a function (@ last update)
+  //     User.findOne({ name: "Joe" }).then((user) => {
+  //       assert(user === null);
+  //       done();
+  //     });
+  //   });
+  // });
+
+  it("delete one user by id", (done) => {
+    User.deleteOne({ _id: joe._id })
+      .then(() => User.findOne({ _id: joe._id }))
+      .then((user) => {
+        assert(user === null);
+        done();
+      });
+  });
+});
---
+++ test/reading_test.js
@@ -0,0 +1,26 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Reading users", () => {
+  let joe; // to make available to all tests
+  beforeEach((done) => {
+    // insert a record into users collection with name of joe
+    joe = new User({ name: "Joe" });
+
+    joe.save().then(() => done());
+  });
+
+  it("finds all users with name of joe", (done) => {
+    User.find({ name: "Joe" }).then((users) => {
+      assert(users[0]._id.toString() === joe._id.toString());
+      done();
+    });
+  });
+
+  it("find a user with a particular id", (done) => {
+    User.findOne({ _id: joe._id }).then((user) => {
+      assert(user.name === "Joe");
+      done();
+    });
+  });
+});
