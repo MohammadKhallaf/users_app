0c40ceca0e1f9b5c632d6d85b3413feb998fe52f
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2024-03-10 10:47:49 +0200

    add update the subdoc

--- test/sub-documents_test.js
+++ test/sub-documents_test.js
@@ -21,4 +21,25 @@ describe("Subdocuments", () => {
       })
       .catch((error) => done(error));
   });
+
+  it("can add subdocuments to an existing records", () => {
+    const joe = new User({
+      name: "JoeWithEightChars",
+      posts: [], // just for the purpose of this test
+    });
+
+    // series of operations => find -> update & save -> find -> test
+    joe
+      .save()
+      .then(() => User.findOne({ name: "JoeWithEightChars" }))
+      .then((user) => {
+        user.posts.push({ title: "PostTitle" });
+        return user.save(); // add to the user model and save it
+      })
+      .then(() => User.findOne({ name: "JoeWithEightChars" }))
+      .then((user) => {
+        assert(user.posts[0].title === "PostTitle");
+        done();
+      });
+  });
 });
