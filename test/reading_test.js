2d60a631f4d401147ca75152e4a9616ecef58dd1
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2024-03-14 10:08:22 +0200

    add sort and pagination

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,12 +2,22 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Reading users", () => {
-  let joe; // to make available to all tests
+  let joe, khaled, sarah, mohamed, jane; // to make available to all tests
   beforeEach((done) => {
     // insert a record into users collection with name of joe
     joe = new User({ name: "JoeWithEightChars" });
+    khaled = new User({ name: "KhaledWithEightChars" });
+    sarah = new User({ name: "SarahWithEightChars" });
+    mohamed = new User({ name: "MohamedWithEightChars" });
+    jane = new User({ name: "JaneWithEightChars" });
 
-    joe.save().then(() => done());
+    Promise.all([
+      joe.save(),
+      khaled.save(),
+      sarah.save(),
+      mohamed.save(),
+      jane.save(),
+    ]).then(() => done());
   });
 
   it("finds all users with name of joe", (done) => {
@@ -22,5 +32,17 @@ describe("Reading users", () => {
       assert(user.name === "JoeWithEightChars");
       done();
     });
+  });
+
+  it("result pagination with skip and limit", (done) => {
+    User.find({}) // find({}) : NO filter criteria
+      .sort({ name: 1 })
+      .skip(1)
+      .limit(3)
+      .then((users) => {
+        console.log(users);
+        assert(users.length === 3);
+        done();
+      });
   });
 });
